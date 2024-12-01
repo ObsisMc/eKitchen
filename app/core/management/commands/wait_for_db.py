a20f40a15108d259052111ac4cb2f3b29ec94a77
zrh_linux <11912614@mail.sustech.edu.cn>
2024-12-01 10:29:25 -0600

    update wait_for_db to ensure the db is totally available

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -4,6 +4,7 @@ from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+from django.db import connections
 
 
 class Command(BaseCommand):
@@ -16,8 +17,10 @@ class Command(BaseCommand):
         while db_up is False:
             try:
                 self.check(databases=['default'])
+                connections['default']  # make sure the connection is available
                 db_up = True
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
+
         self.stdout.write(self.style.SUCCESS('Database available!'))
