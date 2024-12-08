934b3b29d4052711b28dde0290084c2d4e314843
zrh_linux <11912614@mail.sustech.edu.cn>
2024-12-08 17:19:14 -0600

    Image API progressing: config static path

--- app/app/settings.py
+++ app/app/settings.py
@@ -127,8 +127,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+# url to get files
+STATIC_URL = '/static/static/'
+MEDIA_URL = '/static/media/'
+# the real path in the server
+MEDIA_ROOT = "/vol/web/media"
+STATIC_ROOT = "/vol/web/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
